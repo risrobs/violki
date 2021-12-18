b6e9156e2fbc6f24537f8cadc3b58c2fa5c2e756
risrobs <robertivanov22@abv.bg>
2021-12-18 03:09:00 +0200

    k[

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,9 @@ window.addEventListener("DOMContentLoaded", () => {
     alert("💣");
   });
 });
+const box = document.querySelector(".box");
+const boxStyles = getComputedStyle(box);
+const boxMainColor = boxStyles.getPropertyValue("--box-main-color");
+
+const header = document.querySelector("#main-header");
+header.style.setProperty("--header-bg-color", boxMainColor);
